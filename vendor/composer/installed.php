21c25d4e88cba937c3c59437d4dd473d9c9ede41
nikelioum <dimliarop@gmail.com>
2023-10-07 11:59:50 +0300

    Version 1.01

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nikelioum/mypasswordgenerator',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8d4591aee45d299a4090571eaf41ccfd1a3ccd9d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'nikelioum/mypasswordgenerator' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8d4591aee45d299a4090571eaf41ccfd1a3ccd9d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
